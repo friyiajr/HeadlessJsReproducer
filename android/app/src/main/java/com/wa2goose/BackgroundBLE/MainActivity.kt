a14b871ea5fc955f6203ae840bf34877c3e27f35
Daniel Friyia <dfriyia@gmail.com>
2024-04-09 13:25:00 -0400

    Good

--- android/app/src/main/java/com/wa2goose/BackgroundBLE/MainActivity.kt
+++ android/app/src/main/java/com/wa2goose/BackgroundBLE/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.wa2goose.BackgroundBLE
 
 import android.app.ActivityManager
-import android.content.ComponentName
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
 import androidx.annotation.RequiresApi
-import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.wa2goose.BackgroundBLE.PulseCheck.Companion.CHANNEL_ID
 import expo.modules.ReactActivityDelegateWrapper
 import java.util.Timer
 import java.util.TimerTask
@@ -21,40 +20,47 @@ import java.util.TimerTask
 class MainActivity : ReactActivity() {
     private var timer: Timer? = null
 
-    val mServerConn = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName, binder: IBinder) {
-        }
-        override fun onServiceDisconnected(name: ComponentName) {
+    private var notificationManager: NotificationManager? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(null)
+
+        // Start a navigation channel so we can use it to call `startForeground`
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_ID,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel.description = "A Sample Channel";
+        notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager?.createNotificationChannel(channel)
+
+        // Service has to be started in the foreground the first time
+        // it is called
+        val service = Intent(applicationContext, PulseCheck::class.java)
+        val bundle = Bundle()
+        bundle.putString("foo", "bar")
+        service.putExtras(bundle)
+        applicationContext.startForegroundService(service)
+
+        if(timer == null) {
+            timer = Timer()
+            timer?.scheduleAtFixedRate( object : TimerTask() {
+                @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+                override fun run() {
+                    if(!isAppOnForeground(applicationContext)) {
+                        val service = Intent(applicationContext, PulseCheck::class.java)
+                        val bundle = Bundle()
+                        bundle.putString("foo", "bar")
+                        service.putExtras(bundle)
+                        applicationContext.startForegroundService(service)
+                    }
+                }
+            }, 0, 30000)
         }
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-    // Set the theme to AppTheme BEFORE onCreate to support
-    // coloring the background, status bar, and navigation bar.
-    // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
-    super.onCreate(null)
-
-      if(timer == null) {
-          timer = Timer()
-          timer?.scheduleAtFixedRate( object : TimerTask() {
-              @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-              override fun run() {
-
-                      val intent = Intent(applicationContext, PulseReceiver::class.java)
-                          .setAction("com.wa2goose.broadcast.PulseCheck")
-                      sendBroadcast(intent)
-
-//                      val service = Intent(applicationContext, PulseCheck::class.java)
-//                      val bundle = Bundle()
-//                      bundle.putString("foo", "bar")
-//                      service.putExtras(bundle)
-//                      applicationContext.startService(service)
-
-              }
-          }, 0, 30000)
-      }
-  }
 
     private fun isAppOnForeground(context: Context): Boolean {
         /**
@@ -78,43 +84,43 @@ class MainActivity : ReactActivity() {
         super.onResume()
     }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "main"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "main"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return ReactActivityDelegateWrapper(
-          this,
-          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-          object : DefaultReactActivityDelegate(
-              this,
-              mainComponentName,
-              fabricEnabled
-          ){})
-  }
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return ReactActivityDelegateWrapper(
+            this,
+            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+            object : DefaultReactActivityDelegate(
+                this,
+                mainComponentName,
+                fabricEnabled
+            ){})
+    }
 
-  /**
-    * Align the back button behavior with Android S
-    * where moving root activities to background instead of finishing activities.
-    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
-    */
-  override fun invokeDefaultOnBackPressed() {
-      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-          if (!moveTaskToBack(false)) {
-              // For non-root activities, use the default implementation to finish them.
-              super.invokeDefaultOnBackPressed()
-          }
-          return
-      }
+    /**
+     * Align the back button behavior with Android S
+     * where moving root activities to background instead of finishing activities.
+     * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+     */
+    override fun invokeDefaultOnBackPressed() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+            if (!moveTaskToBack(false)) {
+                // For non-root activities, use the default implementation to finish them.
+                super.invokeDefaultOnBackPressed()
+            }
+            return
+        }
 
-      // Use the default back button implementation on Android S
-      // because it's doing more than [Activity.moveTaskToBack] in fact.
-      super.invokeDefaultOnBackPressed()
-  }
+        // Use the default back button implementation on Android S
+        // because it's doing more than [Activity.moveTaskToBack] in fact.
+        super.invokeDefaultOnBackPressed()
+    }
 }
--- android/app/src/main/java/com/wa2goose/BackgroundBLE/PulseCheck.kt
+++ android/app/src/main/java/com/wa2goose/BackgroundBLE/PulseCheck.kt
@@ -1,18 +1,56 @@
 package com.wa2goose.BackgroundBLE
 
-import android.app.ActivityManager
+import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.jstasks.HeadlessJsTaskConfig
 import com.facebook.react.jstasks.NoRetryPolicy
 
-class PulseCheck : HeadlessJsTaskService() {
 
+class PulseCheck : HeadlessJsTaskService() {
+    companion object {
+        val CHANNEL_ID = "MyServiceChannel"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun buildNotification(context: Context): Notification {
+        return NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle("Running in Background")
+            .setContentText("Running in Background")
+            .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
+            .setContentIntent(Intent(context, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+            })
+            .build()
+    }
+
+    var isFirst = true
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if(isFirst) {
+            val notification = buildNotification(applicationContext)
+            ServiceCompat.startForeground(
+                this,
+                1,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            )
+            isFirst = false
+            return START_REDELIVER_INTENT
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getTaskConfig(intent: Intent): HeadlessJsTaskConfig? {
         return intent.extras?.let {
             HeadlessJsTaskConfig(
